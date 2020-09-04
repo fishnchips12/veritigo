d0ef08ce235779180eecb06914e3344084cf5c10
Wilson Cao <thebestpeopleintheworld51@gmail.com>
2020-09-04 23:24:32 +1000

    change

---
+++ site.php
@@ -0,0 +1,22 @@
+<? require "header.html" ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>yet</title>
+    <script src="javascript/site.js"> </script>
+    <link rel="stylesheet" href="style/site.css">
+  </head>
+  <body>
+    <div class="section">
+      <div class="advertisement">
+
+
+</address>
+      </div>
+    </div>
+  </body>
+</html>
+
+<? require "footer.html" ?>
